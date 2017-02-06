f7c4088f6558c127ffd965be62edba6db0269ced
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-05 20:11:51 -0800

    Pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,18 @@
+
+
+#fake_name (name)
+#Split the name into an array using the split function
+#Start by changing the first and last letters using swap
+#Iterate through all the letters in the name
+  #If the letter is a consonant change it the the next possible consonant
+  #To compare make a list of consonants that the list can move onto
+
+
+
+def fake_name (name)
+  name.split('')
+end
+
+
+
+p fake_name("daniel")
