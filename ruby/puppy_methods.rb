6ebe48136519f13d20253a4287255009f885886a
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-07 11:14:21 -0700

    Release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,36 @@
 class Puppy
 
-  def fetch(toy)
+  def self.fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def self.speak(int)
+  	puts "Woof!"*int
+  end
+
+
+  def self.rollover
+  	puts "rollover"
+  end
+
+  def self.dog_years(int)
+  	puts int * 7
+  	
+  end
+
+  def self.sit
+  	puts "sit"
+  end
+
 end
 
+Puppy.fetch('ball')
+
+Puppy.speak(2)
+
+Puppy.rollover
+
+Puppy.dog_years(5)
+
+Puppy.sit
