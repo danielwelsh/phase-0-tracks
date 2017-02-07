5d1c6f6535403d8dd6adbe1714894177a95cdab6
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-06 19:15:50 -0700

    Add puppy description

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+#SPECIES ------------------------
+#Pupius Dogmus
+
+#CHARACTERISTICS ----------------
+#Eyelash length: 2-3 inches
+#Friendly: Yes, very 
+#Eye count: 2
+#Name: varies
+#Color: white and black spotted
+#Skin: Long fur
+
+#BEHAVIOR -----------------------
+#Jump
+#Run
+#Lick
+#Laugh
