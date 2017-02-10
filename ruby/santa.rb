abfb4e6e8613e3ed9efd8703eec5770062c4a6c6
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-09 20:05:14 -0700

    Add Release 0

---
+++ ruby/santa.rb
@@ -0,0 +1,15 @@
+class Santa
+
+	def self.speak
+		puts 'Ho, ho, ho! Haaaapy holidays!'
+	end
+
+	def self.eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}"		
+	end
+
+	def self.initialize
+		puts "Inializing Santa instance..."
+	end
+
+end
