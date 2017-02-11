399d4098f31c41ffe699b251f1fc63658130785c
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-11 14:22:49 -0700

    Add driver pseudocode with class method ideas

---
+++ ruby/6.6/game.rb
@@ -0,0 +1,40 @@
+#Pseudocode for game word guessing game. 
+
+#Create game class
+
+#Create reader and getter variables
+
+
+#Method- Guessing functions
+
+
+
+
+
+
+
+
+
+#Driver Code
+
+#The user is welcomed to the game
+
+#User 1 is prompted to enter a word into the game
+	#Have this blurred out like a password if possible
+
+
+#User 2 enters into the guessing loop of the game. This is limited to the length of the word
+
+	#User 2 is given the length of the word in order to help gauge what they are looking for
+
+		#User 2 is asked if they would like to make a guess of a letter or a word
+			#User 2 makes a guess of a word
+				#If the word is correct they are told so and the game ends
+				#Else if they are wrong they are looped back to give another guess
+			#User 2 makes a guess of a letter
+				#The letter is checked to be include in the word. 
+					#If the letter is in word, fill in the blank
+					#elsif the letter has already been guessed this will not count against the user. the loop should be broken to prevent the counter being added to. 
+					#Else print a message that this letter was not included
+
+
