c455afba6c39e6de9eb90e4b89969df16c1904cf
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-13 09:19:20 -0700

    Massive commit of files that had never been pushed up

--- ruby/practice.rb
+++ ruby/practice.rb
@@ -1,72 +1,7 @@
-  
-
-
-#Method so prob can be checked at multiple points 
-def probchecker(prob)
-
-    #Determines outputs based on all the previous tests
-  if prob == 1
-     puts "Probably not a vampire"
-
-  elsif prob == 2
-     puts "Probably a vampire"
-
-  elsif prob == 3
-     puts "Almost certainly a vampire"
-     
-  elsif prob == 4
-     puts "Definitely a vampire"
-
-  else 
-    puts "Results inconclusive"
-  end
-  
+def hello
+  puts "Why hello there"
+  yield("dan","hannah")
 end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  #The user is told instructions and the list is created. 
-  puts "Type the word 'done' at any time to leave."
-  puts "Please enter any alergies that you may have:"
-  alergies = []
-  input = ""
-  i = 1
-
-  #Asks the user to give input but will break the loop if they enter the correct input. 
-  while input != 'done'
-    puts "Alergy: #{i}"
-    i += 1
-    input = gets.chomp
-
-    #To prevent the word exit being added. 
-    if input != 'done'
-      alergies.push(input)
-    end 
-
-    #Check to see if the user is a vampire
-    if input == "sunshine"
-      prob = 4
-      probchecker(prob)
-      break
-    end 
-
-  end
-
-
-  #Displays all of the alergies the person has. 
-  puts "\nThanks for your input. These are your stated alergies:"
-  puts alergies
-
+hello { |name1, name2| puts "great to meet you #{name1} and #{name2}."}
