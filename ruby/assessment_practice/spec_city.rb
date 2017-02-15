49abd4395b3508da21456a8115056b477c3de6fe
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-15 13:49:59 -0700

    RSpec: Add test for min_city_price.

--- ruby/assessment_practice/spec_city.rb
+++ ruby/assessment_practice/spec_city.rb
@@ -9,7 +9,7 @@ describe City do
   city.houses << house1
   city.houses << house2
 
-    expect(city.avg_home).to eq (425000)
+    expect(city.avg_home).to eq ("The average price of a home is $425000.")
   end
 
   it "maximum price of home that exists" do 
@@ -17,8 +17,15 @@ describe City do
   house2 = House.new(400000,350000,2500)
   city.houses << house1
   city.houses << house2
-    expect(city.max_city_price).to eq(450000)
+    expect(city.max_city_price).to eq("The maximum price of a home is $450000")
   end
 
+  it "minimum price of a home that exists" do 
+  house1 = House.new(450000,400000,2500)
+  house2 = House.new(400000,350000,2500)
+  city.houses << house1
+  city.houses << house2
+  expect(city.min_city_price).to eq("The minimum price of a home is $400000")
+  end
 
 end
