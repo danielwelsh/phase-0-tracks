32806bd70c9b6547fe785c9aaca70eee1181558c
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-15 12:01:28 -0700

    Country: Add psuedocode for the city class

---
+++ ruby/assessment_practice/country.rb
@@ -0,0 +1,16 @@
+
+
+require_relative 'city'
+
+class Country
+	#determine and set getter/setter methods
+
+	#method initialize
+		#creates the instance of a country. 
+		#create the hash/array that stores all the city data. 
+
+	#method read_city
+		#reads in the city data into and instance variable 
+
+
+end
