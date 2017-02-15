27e3a211dab43153f5dac63cb26d03ee7735fa5a
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-15 12:01:00 -0700

    City: Add psuedocode for the city class

---
+++ ruby/assessment_practice/city.rb
@@ -0,0 +1,43 @@
+
+
+
+require_relative 'house'
+
+class City
+	#determine and set setter/getter methods
+
+
+	#method initialize
+		#reads that hash that was created in the house class and creates an instance based on the city symbol
+		#The loop to create all the instances of cities will be done using driver code. 
+
+	#method output_city_data
+		#nicly outputs the other city wide calculations that were done. 
+		#avg_home
+		#max_home
+		#min_home
+
+	#method avg_home
+		#iterates through each home in the city
+			#adds the values up in a holding variable
+		#divides by the number of houses in that city
+
+		#This can be done as a private function
+
+	#method max_home
+		#finds the maximum home value in that city
+		#returns statement that says "The maximum home value in ___ city is ___"
+
+	#method min_home 
+		#finds the minimum home value in that city
+		#returns statement that says "The minimum home value in ___ city is ___"
+
+
+
+
+
+
+
+
+
+end
