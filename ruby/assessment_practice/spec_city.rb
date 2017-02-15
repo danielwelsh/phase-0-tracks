ea97055f9c157cfb372bbef16f08d9a1d3f2f493
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-15 13:26:15 -0700

    RSpec: Change the test for avg_home

--- ruby/assessment_practice/spec_city.rb
+++ ruby/assessment_practice/spec_city.rb
@@ -1,18 +1,17 @@
-require_relative './house'
+require_relative 'city'
 
 describe City do
   let(:city) { City.new }
 
-  it "price per square foot" do
-    house1 = house.new(450000,400000,2500)
-    house2 = house.new(400000,350000,2500)
-    @houses << house1
-    @houses << house2
+  it "houses average price" do
+  house1 = House.new(450000,400000,2500)
+  house2 = House.new(400000,350000,2500)
+  city.houses << house1
+  city.houses << house2
+  puts city.avg_home
 
     expect(city.avg_home).to eq (425000)
   end
 
-  
-  end
 
 end
