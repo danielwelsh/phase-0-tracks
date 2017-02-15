2a376ee90d701ac1179cf637214857d1d294a38b
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-15 12:00:12 -0700

    House: add psuedocode plan

---
+++ ruby/assessment_practice/house.rb
@@ -0,0 +1,30 @@
+#create a hash that is stored in another document.
+#In the driver code of this program there will be a loop that reads in everything and initalizes it 
+
+
+
+class House
+
+	#determine and create setter/getter methods
+
+
+	#method initialize
+		#Creates an instance of each house 
+		#Sets all the instance variables that have been passed in as parameters
+
+	#method add house to list
+		#Takes in external parameters and allows the houses information to be updated
+
+	#method house info
+		#This method prints all the private methods in a nice way
+
+	#method calc_price_per_foot
+		#Allows a calculation to be performed that tells price per square foot. 
+		#This method can be made as a private method that only uses instance variables
+
+	#method capital_gains
+		#Calculation of the year over year loss or appreciation in capital gains that has
+		#affected the house
+		#Like the method before this one is private and does not need to be passed instance variables
+
+end
