64b36c9eae4e75f3864623240ce3735033723138
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-16 13:53:45 -0700

    Country: Rework file by moving single functionalities into class methods.

--- ruby/assessment_practice/country.rb
+++ ruby/assessment_practice/country.rb
@@ -5,16 +5,49 @@ require_relative 'house'
 
 class Country
 	#determine and set getter/setter methods
-	attr_accessor :housing_market
+	attr_accessor :country_housing_market, :cities
 
 	def initialize
 		#create the hash/array that stores all the city data. 
-		@housing_market = []		
+		@country_housing_market = []
+		#Array for prices today
+		@year1_prices = [400000, 500000, 600000, 450000, 800000, 700000, 500000, 320000, 475000, 899000, 999000, 675000, 500000]
+		#Array for last years prices
+		@year2_prices = [450000, 550000, 675000, 800000, 950000, 500000, 375000, 890000, 820000, 789000, 459999, 360000, 780000]
+		#Square foot values that can be used
+		@square_foot = [2500, 4000, 1500, 1000, 700, 5000, 7000, 2200, 1800, 1500, 1100, 3400, 2800]
+		#Cities
+		@cities = {Edmonton: '', Calgary: '', Vancouver: '', Toronto: ''}		
 	end
 		
+
+	def make_city_list 
+		cities_list = []
+		@cities.each do |city, city_instance|
+			puts city
+			cities_list << city.to_s.downcase
+		end
+		cities_list
+	end
+
+
+	def instance_creation
+		#City instance is created
+		@cities.each do |city_name, city_instance|
+			@cities[city_name] = City.new
 	
-	#method read_city
-		#reads in the city data into and instance variable 
+			#House are now created to be placed into the instance of that city
+			(1..10).each do |house|
+				house = House.new(@year1_prices.sample, @year2_prices.sample, @square_foot.sample)
+				@cities[city_name].houses << house
+			end
+		end
+
+		#The City instances is added into the country instance
+		@country_housing_market << @cities
+	end
+
+		
 
 
 end
@@ -26,38 +59,15 @@ end
 
 
 
-#CREATION OF INSTANCES
+#DATA TO CREATE INSTANCES
 
-#Data that is used to create all the instances
-#Array for prices today
-year1_prices = [400000, 500000, 600000, 450000, 800000, 700000, 500000, 320000, 475000, 899000, 999000, 675000, 500000]
-#Array for last years prices
-year2_prices = [450000, 550000, 675000, 800000, 950000, 500000, 375000, 890000, 820000, 789000, 459999, 360000, 780000]
-#Square foot values that can be used
-square_foot = [2500, 4000, 1500, 1000, 700, 5000, 7000, 2200, 1800, 1500, 1100, 3400, 2800]
-#Cities
-cities = {Edmonton: '', Calgary: '', Vancouver: '', Toronto: ''}
+
+
 
 
 #The country instance is created
 canada = Country.new
-
-#City instance is created
-cities.each do |city_name, city_instance|
-	cities[city_name] = City.new
-	
-	#House are now created to be placed into the instance of that city
-	(1..10).each do |house|
-		house = House.new(year1_prices.sample, year2_prices.sample, square_foot.sample)
-		cities[city_name].houses << house
-	end
-
-	
-end
-
-#The City instances is added into the country instance
-canada.housing_market << cities
-
+canada.instance_creation
 
 
 
@@ -75,10 +85,12 @@ canada.housing_market << cities
 #Asks the users if they want a market overview or by the city level
 option = ''
 loop do 
-	puts "\nWould you like a Canadian Market overview or individual city analysis? (enter 'o' or 'c)"
+	puts "\nWould you like a Canadian Market overview or individual city analysis? (enter 'o' or 'c or done)"
 	option = gets.chomp.to_s
 	if option == 'o' || option =='c'
 		break
+	elsif option == 'done'
+		
 	else
 		puts "That is invalid input. Please try again."
 	end
@@ -93,13 +105,10 @@ if option == 'o'
 elsif option == 'c'
 
 	#This list is used to compare input later
-	cities_list = []
+	
 	puts "Please enter the city you would like to anlayze:"
 	puts "Your options are:"
-	cities.each do |city, city_instance|
-		puts city
-		cities_list << city.to_s.downcase
-	end
+	cities_list = canada.make_city_list
 
 	#Takes and validates the user's input
 	option = ''
@@ -107,16 +116,14 @@ elsif option == 'c'
 		puts "\nYour choice:"
 		option = gets.chomp.to_s.downcase
 		if cities_list.include? option 
+			puts "\nIn #{option.capitalize}:"
+			#FIXME: canada.cities needs to be written in a way that would allow the country name to change. 
+			puts "#{canada.cities[option.capitalize.to_sym].output_city}"
 			break
 		else
 			puts "That is invalid input. Please try again."
 		end
 	end
-
-	puts "In #{option.capitalize} #{cities[option.capitalize.to_sym].output_city}"
-
-
-
 else
 end
 	
