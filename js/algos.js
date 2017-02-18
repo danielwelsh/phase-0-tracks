5d1cc14180557c8fc042ea4d01625eec3fd08c87
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-18 16:21:50 -0700

    Algos: Add pseudocode and set up base function

---
+++ js/algos.js
@@ -0,0 +1,42 @@
+// Longest string function
+
+// iterate through each item in the list
+	// Store the length value in a new array in the same index position as the string that was anlayzed. 
+
+	// loop through the array of lengths
+		// Take the first index value
+			// If char is > than next char move to next char
+			// If char is < than next char swap the characsublter being compared to the next
+
+
+
+function longest(string_array) {
+	var array = []
+	for (var i = 0; i < string_array.length; i++) {
+		array.push(string_array[i].length);
+	}
+	// Here we store the index key of the current highest
+	longest_string = 0
+	if ()
+	return array;
+}
+
+
+
+console.log(longest(['hello', 'bye', 'yolo']))
+
+
+
+// for (var key in mazda) {
+//  if (mazda.hasOwnProperty(key)) {
+//    console.log(key + " -> " + mazda[key]);
+//  }
+// }
+
+// for (var key in mazda) {
+//    console.log(key + " -> " + mazda[key]);
+//  }
+ 
+// for (var key in mazda) {
+//    console.log(this.key + " -> " + this.mazda[key]);
+//  }
