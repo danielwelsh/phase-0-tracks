9a17cdb4a703121f873c88b3ff62316f5eb9cdf7
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-17 18:48:58 -0700

    Add if statement for printout

--- js/explore.js
+++ js/explore.js
@@ -3,6 +3,10 @@
 // 		add that letter to a holder variable 
 
 
+// reverse function
+// iterate through each character in the string starting at the last character and moving to the first character
+
+
 function reverse(string) {
   var newString = ''
   for (var i = string.length-1; i > -1; i = i - 1) {
@@ -12,6 +16,14 @@ function reverse(string) {
 }
 
 
+var name = reverse('daniel');
 
-console.log(reverse('daniel'));
+if ( 1 == 1 ) {
+	console.log(name);
+} else {
+	console.log("Nothing");
+}
+
+
+
 
