eb75f4422ab4b8c46fd5378a01102591f437cee6
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-18 21:01:51 -0700

    Algos: add function pair(obj1, obj2)

--- js/algos.js
+++ js/algos.js
@@ -18,8 +18,8 @@ function longest(string_array) {
 	}
 	// Store the index position of the longest word
 	var longest = Math.max.apply(null, lengthArray);
-	index = lengthArray.indexOf(longest)
-	return string_array[index]
+	index = lengthArray.indexOf(longest);
+	return string_array[index];
 
 	// Start with index position 1, loop over all the index positions
 	
@@ -27,22 +27,54 @@ function longest(string_array) {
 }
 
 
-console.log(longest(['hello', 'bye', 'yolo']))
-console.log(longest(['hello', 'bye', 'yolo', 'hockey', 'how are you']))
-console.log(longest(['hockey', 'football', 'soccer', 'the purple cow']))
+function size(obj) {
+	var size = 0;
+	for (var key in obj) {
+ 		size += 1;
+		}
+	return size;
+	}	
 
 
 
-// for (var key in mazda) {
-//  if (mazda.hasOwnProperty(key)) {
-//    console.log(key + " -> " + mazda[key]);
-//  }
-// }
+function pair(obj1, obj2) {
+	// Iterate through the first key-value pair in hash1 and hash2
+	var keys1 = Object.keys(obj1)
+	var keys2 = Object.keys(obj2)
+	
+	for (i = 0; i < keys1.length; i++) {
+		if (obj1[keys1[i]] == obj2[keys2[i]]) {
+			//If it finds a match it will break the loop and return true
+			return true
+		}
 
-// for (var key in mazda) {
-//    console.log(key + " -> " + mazda[key]);
-//  }
- 
-// for (var key in mazda) {
-//    console.log(this.key + " -> " + this.mazda[key]);
-//  }
+	}
+	return false
+		// if values are = return true
+		// if values are != return false
+}
+
+
+
+
+var obj1 = {name: "Steven", age: 54}
+var obj2 = {name: "Tamir", age: 54}
+
+
+console.log(pair(obj1,obj2))
+
+
+
+
+
+
+
+
+
+
+
+// console.log(longest(['hello', 'bye', 'yolo']))
+// console.log(longest(['hello', 'bye', 'yolo', 'hockey', 'how are you']))
+// console.log(longest(['hockey', 'football', 'soccer', 'the purple cow']))
+
+
