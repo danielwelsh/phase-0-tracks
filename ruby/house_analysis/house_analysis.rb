e61b44bf9524d579e2a5729ae232ff02703410c4
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-27 03:00:15 -0700

    House_analysis: Add Driver code, bug when entering toronto

--- ruby/house_analysis/house_analysis.rb
+++ ruby/house_analysis/house_analysis.rb
@@ -76,9 +76,10 @@ class Housing
 	def market_appriciation(market_city)
 		
 		#Find the FOREIGN KEY VALUE for that city
-		city = @db.execute("SELECT cities.id FROM cities WHERE city = '#{user_input}';")
+		city = @db.execute("SELECT cities.id FROM cities WHERE city = '#{market_city}';")
 		city = city[0]
 		city = city['id']
+		
 
 
 		#Return all data for that city
@@ -93,9 +94,9 @@ class Housing
 			year_1_total += house['year_1']
 			year_2_total += house['year_2']
 		end
-		p year_1_total
-		p year_2_total
+	
 		percent = ((year_2_total.to_f - year_1_total.to_f) / year_2_total.to_f) * 100
+		"The average change in prices in the last year was #{percent} percent."
 	end
 
 
@@ -103,7 +104,7 @@ class Housing
 
 	def market_per_foot(market_city)
 		#Find the FOREIGN KEY VALUE for that city
-		city = @db.execute("SELECT cities.id FROM cities WHERE city = '#{user_input}';")
+		city = @db.execute("SELECT cities.id FROM cities WHERE city = '#{market_city}';")
 		city = city[0]
 		city = city['id']
 
@@ -120,9 +121,18 @@ class Housing
 			square_feet += house['square_feet']
 		end
 		price_per_foot = year_1_total / square_feet
+
+		"The average price per square foot is $#{price_per_foot} "
 	end
 
-
+	def print_cities
+		cities = @db.execute("SELECT cities.city FROM cities")
+		cities_array = []
+		cities.each_with_index do |city, index|
+			cities_array << cities[index]['city']
+		end
+		cities_array
+	end
 
 end
 
@@ -135,7 +145,57 @@ end
 
 
 house = Housing.new
-p house.market_appriciation("Edmonton")
+
+
+
+#DRIVER CODE
+
+puts "Welcome to the Canadian Housing prices database."
+
+
+#STARTS LOOP TO ALLOW USER TO USE INTERFACE AS MANY TIMES AS THEY WOULD LIKE. 
+option = ''
+loop do 
+	puts "\nYou you like an analysis on a market? (enter 'YES' or 'DONE')"
+	option = gets.chomp.to_s
+	
+	#Conditional branch that takes user down desired branch
+	if option == 'YES' 
+		
+			#This list is used to compare input later
+			
+			puts "\nPlease enter the city you would like to anlayze:"
+			puts "Your options are:"
+			puts house.print_cities
+			cities_list = house.print_cities
+			# puts cities_list
+
+			#Takes and validates the user's input
+			option = ''
+			loop do 
+				puts "\nYour choice:"
+				option = gets.chomp.to_s
+				if cities_list.include? option 
+					puts "\nIn #{option.capitalize}:" 
+					puts "#{house.market_per_foot(option)}"
+					puts "#{house.market_appriciation(option)}"
+					break
+				else
+					puts "That is invalid input. Please try again."
+				end
+			end
+
+
+	#Breaks loop when done
+	elsif option == 'DONE'
+		break
+	#Else gets the user to enter input again. 	
+	else
+		puts "That is invalid input. Please try again."
+	end
+
+
+end
 
 
 
