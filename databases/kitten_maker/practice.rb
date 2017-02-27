6deb934307aeeed979cc30a849930e72f6dfad6c
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-27 16:00:08 -0700

    Pushing up old files

--- databases/kitten_maker/practice.rb
+++ databases/kitten_maker/practice.rb
@@ -1,4 +1,29 @@
 require 'sqlite3'
 require 'faker'
 
-db = SQLite3::Database.new("kittens.db")
+db = SQLite3::Database.new("kittens_practice.db")
+db.results_as_hash = true
+
+create_table_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS kittens(
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		age INT
+	)
+SQL
+
+db.execute(create_table_cmd)
+
+db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
+kittens = db.execute('SELECT * FROM kittens')
+puts kittens.class
+p kittens
+
+
+kittens.each |kitten|
+	puts "#{kitten['name'] is #{kitten['age']}"
+end
+
+def create_kitten(db, name, age)
+	db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
+end
