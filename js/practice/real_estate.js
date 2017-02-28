3c01d1411f35c3cb337942107beccd7a7e15e869
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-28 15:56:42 -0700

    Real Estate: Add loop to create 100 house object. All prices are the same.

--- js/practice/real_estate.js
+++ js/practice/real_estate.js
@@ -34,3 +34,20 @@ function House(year1, year2, sq_feet) {
 }
 
 
+var house = new House(199, 200, 20000)
+console.log(house)
+
+
+//Data that is used by the loop to populate the housing information. 
+var year1Array = [400000, 500000, 450000, 650000, 700000, 800000]
+var year2Array = [475000, 600000, 560000, 567000, 820000, 750000]
+var sq_feetArray = [2500, 1500, 800, 500, 2000, 2100]
+var houseArray = []
+
+
+// Create a loop that generates many different houses and stores them in houseList
+for (var x = 0; x < 100; x++) {
+	houseArray.push(house[x] = new House(199, 200, 2000))
+}
+
+console.log(houseArray)
