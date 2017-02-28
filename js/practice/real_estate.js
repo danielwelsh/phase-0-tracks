90dca1b6b67e792cba6befa7095b93e3aec489ec
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-28 16:47:15 -0700

    Real Estate:Add appriciation function. Red --> Green

--- js/practice/real_estate.js
+++ js/practice/real_estate.js
@@ -8,11 +8,6 @@
 
 
 
-// function to calculate average house valie
-// function to calculate year over year appriciation
-
-
-
 
 
 // Creating Constructer Functions
@@ -34,13 +29,38 @@ function House(year1, year2, sq_feet) {
 }
 
 
+function appriciation(houseArray) {
+	//Declare value variables
+	var year1_total = 0
+	var year2_total = 0
+
+
+	//Sum the total value of homes in year 1 and 2
+	for (var i = 0; i < houseArray.length; i++) {
+		year1_total += houseArray[i]['year1'];
+		year2_total += houseArray[i]['year2'];
+	}
+
+	//Find the percent increase from year to year
+	percent = (year2_total - year1_total) / year2_total
+	percent = Math.round(percent * 100) 
+	//Return statement that declares what the percent increase in the market was
+	return "The housing prices changed by " + percent + "% last year."
+}
+	
+
+	
+	
+
+
+
 var house = new House(199, 200, 20000)
 console.log(house)
 
 
 //Data that is used by the loop to populate the housing information. 
-var year1Array = [400000, 500000, 450000, 650000, 700000, 800000]
-var year2Array = [475000, 600000, 560000, 567000, 820000, 750000]
+var year1Array = [400000, 454000, 450000, 650000, 700000, 800000]
+var year2Array = [475000, 600000, 560000, 567000, 820000, 870000]
 var sq_feetArray = [2500, 1500, 800, 500, 2000, 2100]
 var houseArray = []
 
@@ -50,4 +70,5 @@ for (var x = 0; x < 100; x++) {
 	houseArray.push(house[x] = new House(year1Array[Math.floor(Math.random() * year1Array.length)], year2Array[Math.floor(Math.random() * year2Array.length)], sq_feetArray[Math.floor(Math.random() * sq_feetArray.length)]))
 }
 
-console.log(houseArray)
+
+console.log(appriciation(houseArray))
