4133f6df461d479ef6b683aa9ea5e7413f94c3c3
Daniel Welsh <danielwelsh@borealproductions.com>
2017-02-28 15:27:36 -0700

    Create file and add comments about constructor functions and JS

---
+++ js/practice/real_estate.js
@@ -0,0 +1,28 @@
+// Create data structures and variables to read in info
+// Create loops to read the info into other data structures
+// Create JS instances of each house
+
+// There are no hashes in JS but there are object which act in a similar nature
+// Object in JS behave somewhat likes clases tho they do not ahve the complexity and features of classes in Ruby
+// Objects can be produced from a blueprint or prototype, loosly similar to the way instances are made in ruby
+
+
+
+// function to calculate average house valie
+// function to calculate year over year appriciation
+
+
+
+
+
+// Creating Constructer Functions
+//		Contructer functions have capialized names
+//		this. refers to the present object being manipulated
+//		Using this. can be used to do the same thing as @name = name 
+//		This is done like this.name = name
+
+
+
+// Create a constructor function that creates homes
+
+function House(year1, year2, sq_feet)
